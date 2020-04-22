00e9a74f603fbb4fd9503b9644c9a7ae85f2d58e
quanchao <lqc@gatech.edu>
2020-04-21 20:11:47 -0400

    Added the tweetPart

---
+++ js/tweet.js
@@ -0,0 +1,23 @@
+var tweetData = [
+    {
+        "longitude": -74.00,
+        "latitude": 40.42,
+        "message": "Confusion prevails over how many people in Iran have been infected, and how many have died, from the coronavirus.\u00a0 https://t.co/FiA8Oxu6uR"
+    },
+    {
+        "longitude": -77.0369,
+        "latitude": 38.9072,
+        "message": "Second domestic coronavirus infection possibly due to sloppy handling of infected people brought in through Travis\u2026 https://t.co/oP3gUkOovQ"
+    },
+    {
+        "longitude": -79.0193,
+        "latitude": 35.7596,
+        "message": " Mexico confirms first 2 coronavirus cases, health official says https://t.co/nVJr4ILb6A #FoxNews"
+    },
+    {
+        "longitude": -81.5158,
+        "latitude": 27.6648,
+        "message": " Corona beer does not cause coronavirus, but a survey shows that US \u2018purchase intent\u2019 is down https://t.co/QqIskHq1Cj"
+    },
+
+]
